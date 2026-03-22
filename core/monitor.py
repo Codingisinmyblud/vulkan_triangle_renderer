aa2649a290dbbb253929b4c23f0318f43f359082
Qasim Khawaja <dotboss1010@gmail.com>
2026-03-22 09:12:00 +0100

    Hook up monitor to adb collectors

--- core/monitor.py
+++ core/monitor.py
@@ -1,4 +1,8 @@
 import psutil
+from collector.cpu import get_app_cpu
+from collector.memory import get_app_memory
+from collector.gfx import get_fps
+from analyzer.rules import analyze_metrics
 
 class EmulatorMonitor:
     def __init__(self, package_name):
@@ -9,3 +13,12 @@ class EmulatorMonitor:
             "cpu": psutil.cpu_percent(interval=None),
             "memory": psutil.virtual_memory().percent
         }
+
+    def get_emulator_metrics(self):
+        cpu = get_app_cpu(self.package_name)
+        mem = get_app_memory(self.package_name)
+        fps = get_fps(self.package_name)
+        return {"cpu": cpu, "memory": mem, "fps": fps}
+
+    def analyze(self, metrics):
+        return analyze_metrics(metrics["cpu"], metrics["memory"], metrics["fps"])
